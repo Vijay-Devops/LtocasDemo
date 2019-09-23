e4842b50104d481b36b4d91836748556ac84731f
balachander28 <54208802+balachander28@users.noreply.github.com>
2019-09-23 11:08:18 +0530

    hi i added a new class in Ltocas.java

--- src/Ltocas.java
+++ src/Ltocas.java
@@ -4,7 +4,13 @@ public class Ltocas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		Aruna.demo();
 
 	}
 
+}
+class Aruna{
+static void demo() {
+		System.out.println("i am aruna");
+	}
 }
