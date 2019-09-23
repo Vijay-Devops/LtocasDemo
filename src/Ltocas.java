f78e7db60c2e0e185167555453279ecee149e438
LijoBF <lijo@lijo-PC>
2019-09-23 11:12:05 +0530

    commit

--- src/Ltocas.java
+++ src/Ltocas.java
@@ -1,6 +1,9 @@
 
 public class Ltocas {
-
+	public void print() {
+		System.out.println("print");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
